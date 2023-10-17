f031574f1d0fcda7feccaed704e03577a62bfb73
JDN89 <jan.de.niels@gmail.com>
2023-10-17 21:32:57 +0200

    reading args from cli

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -1,3 +1,9 @@
+use ::std::env;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    // First argument is the path of the executable
+    let query = &args[1];
+    let file_path = &args[2];
+    println!("Searching for: {} in file: {}", query, file_path);
 }
