4be9997071a7034370bf501ac442d2c586b1ccd1
JDN89 <jan.de.niels@gmail.com>
2023-10-18 22:25:25 +0200

    refactoring

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -1,9 +1,38 @@
 use ::std::env;
+use ::std::fs;
+
+// fix 4 problems:
+// 1 single responsability
+// 2 query and file_path are config vars contents is used to preform programs logic -> group config
+// vars in on struct
+// 3 expect used to print same error always
+// error handling code in one place
 
 fn main() {
     let args: Vec<String> = env::args().collect();
+    let config = Config::new(&args);
+
     // First argument is the path of the executable
-    let query = &args[1];
-    let file_path = &args[2];
-    println!("Searching for: {} in file: {}", query, file_path);
+    println!(
+        "Searching for: {} in file: {}",
+        config.query, config.file_path
+    );
+    let contents =
+        fs::read_to_string(config.file_path).expect("Should have been able to read the file");
+
+    println!("With text:\n {contents}");
+}
+
+#[derive(Debug)]
+struct Config {
+    query: String,
+    file_path: String,
+}
+
+impl Config {
+    fn new(args: &[String]) -> Config {
+        let query = args[1].clone();
+        let file_path = args[2].clone();
+        Config { query, file_path }
+    }
 }
