ff433fc97540d8f5bb915ccb08d397d2cd6adef7
JDN89 <jan.de.niels@gmail.com>
2023-12-16 14:48:51 +0100

    refactor

--- rlox/README.md
+++ rlox/README.md
@@ -15,6 +15,8 @@ Project contains executable for Lox and AST struct generator
 
 ## TODO
 - test al scenarios: does the interpreter work 
+- rename  error enum types
+- fix error reporting of the InterpreterError: THE OPERATOR IS THE TOKEN, include always the operator in the error reporting! We beed location. How do I access the token that contains the location of where the error occured.
 - impl PartialOrd -> look at exmaple advent of code day 2 for an example and write unit tests
 - see if you can replace match in some places with __if let__, better to use in combination with enums where I'm only interested in one specific enum field
 - why does the  Parse error get priority to the token error? >~  [line 0, lexeme '~'] Error: Expcted expression. Should I create report parse_error_fn and report the parse error on the place where we call it in case of an error? Create seperate ParseErro Struct? or place SourceCodeError and ParseError in LoxError enum? *look into*
--- rlox/src/lox/lib.rs
+++ rlox/src/lox/lib.rs
@@ -2,30 +2,38 @@ use std::io::{BufRead, Write};
 use std::{fs, io, process};
 
 use ast_printer::AstPrinter;
-pub use lox_error::*;
-use scanner::Scanner;
-use parser::Parser;
 use interpreter::Interpreter;
+pub use lox_error::*;
+use parser::Parser;
+use scanner::Scanner;
 
 mod ast_printer;
 mod expr;
+mod interpreter;
 mod lox_error;
 mod parser;
 mod scanner;
 mod token;
 mod token_type;
-mod interpreter;
 
 // possible need of converson to Box<dyn Error>
 pub fn run_file(file_path: &str) -> Result<(), io::Error> {
     let contents = fs::read_to_string(file_path)?;
     if let Err(e) = run(&contents) {
         match e {
-            LoxError::Interpreter(e) => e.report(),
-            LoxError::ParserError(e) => e.report(),
-            LoxError::ScannerError(e) => e.report(),
+            LoxError::ScannerError(e) => {
+                e.report();
+                process::exit(65)
+            }
+            LoxError::ParserError(e) => {
+                e.report();
+                process::exit(65)
+            }
+            LoxError::Interpreter(e) => {
+                e.report();
+                process::exit(70)
+            }
         }
-        process::exit(65);
     }
     Ok(())
 }
@@ -48,24 +56,24 @@ pub fn run_prompt() -> Result<(), io::Error> {
         }
 
         if let Err(e) = run(&buf) {
-        match e {
-            LoxError::Interpreter(e) => e.report(),
-            LoxError::ParserError(e) => e.report(),
-            LoxError::ScannerError(e) => e.report(),
-        }
+            match e {
+                LoxError::Interpreter(e) => e.report(),
+                LoxError::ParserError(e) => e.report(),
+                LoxError::ScannerError(e) => e.report(),
+            }
         }
     }
 
     Ok(())
 }
 
-// TODO: test al scenarios: does the interpreter work 
+// TODO: test al scenarios: does the interpreter work
 fn run(source: &String) -> Result<(), LoxError> {
     let mut scanner = Scanner::build_scanner(source);
     let tokens = scanner.scan_tokens()?;
-    let mut  parser = Parser::build_parser(tokens.clone());
+    let mut parser = Parser::build_parser(tokens.clone());
     let expression = parser.parse()?;
-    let interpreter = Interpreter{};
+    let interpreter = Interpreter {};
     interpreter.interpret(&Box::new(expression))?;
     Ok(())
 }
