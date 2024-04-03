f4db7254bfc15793501b09cacdfa94bf64c2726b
JDN89 <jan.de.niels@gmail.com>
2024-04-03 22:18:14 +0200

    init chapter 19

---
+++ clox/object.h
@@ -0,0 +1,37 @@
+#ifndef clox_object_h
+#define clox_object_h
+
+#include "common.h"
+#include "value.h"
+
+#define OBJ_TYPE(value) (AS_OBJ(value)->type)
+
+#define IS_STRING(value) isObjType(value, OBJ_STRING)
+
+// These two macros take a Value that is expected to contain a pointer to a
+// valid ObjString on the heap. The first one returns the ObjString* pointer.
+// The second one steps through that to return the character array itself, since
+// that’s often what we’ll end up needing.
+
+#define AS_STRING(value) ((ObjString *)AS_OBJ(value))
+#define AS_CSTRING(value) (((ObjString *)AS_OBJ(value))->chars)
+
+typedef enum {
+  OBJ_STRING,
+} ObjType;
+
+struct Obj {
+  ObjType type;
+};
+
+struct ObjString {
+  Obj obj;
+  int length;
+  char *chars;
+};
+
+static inline bool isObjType(Value value, ObjType type) {
+  return IS_OBJ(value) && AS_OBJ(value)->type == type;
+}
+
+#endif
--- clox/value.h
+++ clox/value.h
@@ -3,13 +3,20 @@
 
 #include "common.h"
 
-typedef enum { VAL_BOOL, VAL_NIL, VAL_NUMBER } ValueType;
+typedef struct Obj obj;
+typedef struct ObjString ObjString;
+
+typedef enum { VAL_BOOL, VAL_NIL, VAL_NUMBER, VAL_OBJ } ValueType;
 
 typedef struct {
   ValueType type;
+  // tagged union. The size of the union is the size of it largest field
+  // the fields overlap in memory. But caution, don't have boolean and later
+  // access it as a number!
   union {
     bool boolean;
     double number;
+    Obj *obj;
   } as;
 } Value;
 
@@ -17,15 +24,23 @@ typedef struct {
 #define IS_BOOL(value) ((value).type == VAL_BOOL)
 #define IS_NIL(value) ((value).type == VAL_NIL)
 #define IS_NUMBER(value) ((value).type == VAL_NUMBER)
+#define IS_OBJ(value) ((value).type == VAL_OBJ)
 
 // Convert Clox value to a native C value
 #define AS_BOOL(value) ((value).as.boolean)
 #define AS_NUMBER(value) ((value).as.number)
+#define AS_OBJ(value) ((value).as.obj)
 
 // convert native C value to a Clox value
 #define BOOL_VAL(value) ((Value){VAL_BOOL, {.boolean = value}})
 #define NIL_VAL ((Value){VAL_NIL, {.number = 0}})
 #define NUMBER_VAL(value) ((Value){VAL_NUMBER, {.number = value}})
+// (Obj*) object: This casts object, which is assumed to be a pointer, to type
+// Obj*. It essentially converts the pointer to a pointer of type Obj.*:
+// The asterisk (*) is the dereference operator. It is used to access the value
+// pointed to by a pointer. In this context, it's used to dereference the
+// pointer object, accessing the actual Obj value it points to.
+#define OBJ_VAL(object) ((Value){VAL_OBJ, {.obj = *(Obj)object}})
 
 typedef struct {
   int capacity;
