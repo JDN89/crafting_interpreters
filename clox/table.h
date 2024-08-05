0b86c850ebab37ddabd4376626840c3cad2ee15a
JDN89 <jan.de.niels@gmail.com>
2024-08-05 22:12:50 +0200

    adding and finding values in the hashtable

--- clox/table.h
+++ clox/table.h
@@ -16,6 +16,13 @@ typedef struct {
 } Table;
 
 void initTable(Table *table);
+
 void freeTable(Table *table);
+
+bool tableGet(Table *table, ObjString *key, Value *value);
+
+bool tableSet(Table *table, ObjString *key, Value value);
+
+void tableAddAll(Table *from, Table *to);
 
 #endif
